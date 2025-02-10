18699d18fe605561a2fb3dcf5562a1d0e7ce408b
Jesús Erro <olagato@gmail.com>
2025-02-10 17:11:35 +0100

    chore: commit save point

--- cleaning.py
+++ cleaning.py
@@ -1,2 +1,12 @@
+import pandas as pd
+
+def load_data(file_path):
+    df = pd.read_excel(file_path, engine="xlrd")
+    return df
+
 def main():
-    print("Hello World!")
+    print("Cleaning data...")
+    
+    df = load_data('data/GSAF5.xls')  # Cargamos el archivo de datos
+
+    return df.head()
